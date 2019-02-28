0386c0e4d47da272374bcc255be3510b76fbec54
Michael Bishop <michael@goodworx.co.za>
2019-02-28 21:37:26 +0200

    Hymns

--- src/components/Hymnbook.vue
+++ src/components/Hymnbook.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="q-mt-xs layout-padding">
     <p class="text-center caption">Songs and liturgy</p>
-    <q-list class="no-border" v-if="songs">
-      <div class="text-center">Songs <q-icon size="sm" name="fas fa-music"/>
+    <q-list class="no-border">
+      <div v-if="songs" class="text-center">Songs <q-icon size="sm" name="fas fa-music"/>
         <q-item v-for="song in songs" :key="song.id" :to="'/hymns/' + song.id">
           <q-item-main>
             <div style="color:#4d7227" class="text-center">{{song.title}}</div>
           </q-item-main>
         </q-item>
       </div>
-      <div class="q-mt-md text-center">Liturgy <q-icon size="sm" name="fas fa-list"/>
+      <div v-if="liturgies" class="q-mt-md text-center">Liturgy <q-icon size="sm" name="fas fa-list"/>
         <q-item v-for="liturgy in liturgies" :key="liturgy.id" :to="'/hymns/' + liturgy.id">
           <q-item-main>
             <div style="color:#4d7227" class="text-center">{{liturgy.title}}</div>
