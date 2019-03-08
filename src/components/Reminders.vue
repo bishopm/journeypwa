b84cf3f5e77754d07d29e74b7d218b20264c2746
Michael Bishop <michael@goodworx.co.za>
2019-03-08 13:49:16 +0200

    Formatting reminders

--- src/components/Reminders.vue
+++ src/components/Reminders.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="q-mt-xs">
-    <q-list v-if="notifications" no-border>
+    <q-list striped v-if="notifications" no-border>
       <q-list-header class="text-center">Messages</q-list-header>
       <q-item v-for="notification in notifications" :key="notification.id">
-        <q-item-main class="text-justify">{{notification.message}}</q-item-main>
-        <q-item-side @click.native="deleteme(notification.id)" class="cursor-pointer q-pl-sm text-center" color="red" icon="fa fa-times"><br><small>{{notification.created_at.slice(0,10)}}<br>{{notification.created_at.slice(11)}}</small></q-item-side>
+        <q-item-main class="text-justify"><small v-html="notification.message"></small></q-item-main>
+        <q-item-side @click.native="deleteme(notification.id)" class="cursor-pointer q-pl-sm text-center" color="red" icon="fa fa-times"><br><small>{{notification.ndate}}<br>{{notification.ntime}}</small></q-item-side>
       </q-item>
     </q-list>
     <div class="q-ma-md">{{message}}</div>
@@ -26,7 +26,15 @@ export default {
         individual: this.$store.state.individual.id
       })
       .then(response => {
-        this.notifications = response.data
+        for (var nndx in response.data) {
+          var newitem = {
+            id: response.data[nndx].id,
+            message: response.data[nndx].message.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+            ndate: response.data[nndx].created_at.slice(0, 10),
+            ntime: response.data[nndx].created_at.slice(11)
+          }
+          this.notifications.push(newitem)
+        }
         if (!this.notifications.length) {
           this.message = 'You have no messages'
         }
@@ -40,6 +48,9 @@ export default {
       for (var ndx in this.notifications) {
         if (this.notifications[ndx].id === id) {
           this.notifications.splice(ndx, 1)
+        }
+        if (!this.notifications.length) {
+          this.message = 'You have no messages'
         }
       }
       this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
