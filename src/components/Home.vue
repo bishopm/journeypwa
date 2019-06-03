17d24da009324502651d123d638bd4afdcf2c1b7
Michael Bishop <michael@bishop.net.za>
2019-06-03 21:55:51 +0200

    Changing login again

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,61 +1,5 @@
 <template>
   <div class="q-ma-md">
-    <div class="caption text-center q-mt-md">
-      <div v-if="!phoneverified">
-        <q-btn color="secondary" to="phoneverification">Please verify your phone number</q-btn>
-        <q-btn class="q-ml-sm" label="Why?" color="primary" @click="persistent = true" />
-        <q-dialog v-model="persistent" persistent transition-show="scale" transition-hide="scale">
-          <q-card class="bg-primary text-white">
-            <q-card-section>
-              You don't have to verify your number, but if you do, you'll be able to:<br><br>
-              <li>update your contact details,</li>
-              <li>sign up for events*,</li>
-              <li>view profile pictures of members of your congregation*</li>
-              <li>receive reminders when you are on duty or preaching on a Sunday*, and</li>
-              <li>view your personal giving records.*</li>
-              <li>In addition, you can subscribe to additional blogs / sermon podcasts</li><br>
-              <small>*these features assume that someone in your church is set up as an administrator using the ChurchNet system.</small>
-            </q-card-section>
-            <q-card-actions align="center" class="bg-white text-teal">
-              <q-btn flat label="OK" v-close-popup />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-      <div v-if="!$q.localStorage.getItem('JOURNEY_Individual') && phoneverified">
-        <div v-if="society">
-          Welcome, {{phoneverified}}<br>
-          <q-btn to="adduser" color="secondary">Update my details</q-btn>
-        </div>
-        <q-btn v-else @click="socmodal = true" color="secondary">Which church do you belong to?</q-btn>
-        <q-dialog v-model="socmodal" persistent transition-show="scale" transition-hide="scale">
-          <q-card class="bg-primary text-white" style="width: 400px">
-            <q-card-section>
-              <div class="text-h6">Find your church</div>
-            </q-card-section>
-            <q-card-section class="bg-white">
-              <div class="q-gutter-md row">
-                <q-select outlined v-model="church" label="Church" :options="[{label: 'Methodist Church of Southern Africa', value: 'mcsa'}]" style="width:100%;" emit-value map-options/>
-                <q-select autofocus placeholder="Type here to find or change your church" outlined v-model="mySociety" use-input input-debounce="0" :options="filteredSocieties" @filter="filterFn" clearable style="width:100%;">
-                  <template v-slot:no-option>
-                    <q-item>
-                      <q-item-section class="text-grey">
-                        No results
-                      </q-item-section>
-                    </q-item>
-                  </template>
-                </q-select>
-              </div>
-            </q-card-section>
-            <q-card-actions align="right" class="bg-white text-black">
-              <q-btn class="bg-white text-primary" to="addchurch" flat label="I can't find my church" v-close-popup/>
-              <q-btn class="bg-grey text-white" flat label="Cancel" v-close-popup/>
-              <q-btn class="bg-primary text-white" flat label="OK" @click="setSociety"/>
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-    </div>
     <div class="row q-mt-lg">
       <div class="col-6 text-center q-mb-md">
         <div class="q-mb-sm caption">Sunday's readings</div>
@@ -76,9 +20,10 @@
         </router-link>
       </div>
       <div v-if="$store.getters.getMenu('diary') && $store.state.feeditems.diary.length" class="col-6 text-center q-mb-md">
-        <div class="q-mb-sm caption">Calendar <q-btn size="sm" color="secondary" round>{{$store.state.feeditems.diary.length}}</q-btn></div>
         <router-link to="/diary" class="text-white" style="text-decoration:none;">
+          <div class="q-mb-sm caption text-black">Calendar</div>
           <q-icon class="text-primary" name="fas fa-fw fa-calendar-alt" size="3rem" />
+          <q-btn style="vertical-align:top" color="secondary" size="xs" class="text-black" round>{{$store.state.feeditems.diary.length}}</q-btn>
         </router-link>
       </div>
       <div v-if="$store.getters.getMenu('blog')" class="col-6 text-center q-mb-md">
@@ -113,14 +58,16 @@
       </div>
       <div v-if="$store.getters.getMenu('reminders') && $store.state.feeditems.reminders.length" class="col-6 text-center q-mb-md">
         <router-link to="/reminders" class="text-white" style="text-decoration:none;">
-          <div class="q-mb-sm text-black caption">Inbox <q-btn size="sm" color="secondary" round>{{$store.state.feeditems.reminders.length}}</q-btn></div>
+          <div class="q-mb-sm text-black caption">Inbox</div>
           <q-icon class="text-primary" name="fas fa-fw fa-inbox" size="3rem" />
+          <q-btn style="vertical-align:top" size="xs" color="secondary" round>{{$store.state.feeditems.reminders.length}}</q-btn>
         </router-link>
       </div>
       <div v-if="$store.getters.getMenu('events') && $store.state.feeditems.events.length" class="col-6 text-center q-mb-md">
         <router-link to="/events" class="text-white" style="text-decoration:none;">
-          <div class="q-mb-sm text-black caption">Event sign-up <q-btn size="sm" color="secondary" round>{{$store.state.feeditems.events.length}}</q-btn></div>
+          <div class="q-mb-sm text-black caption">Event sign-up</div>
           <q-icon class="text-primary" name="fas fa-fw fa-clipboard" size="3rem" />
+          <q-btn style="vertical-align:top" size="xs" color="secondary" round>{{$store.state.feeditems.events.length}}</q-btn>
         </router-link>
       </div>
     </div>
@@ -128,110 +75,7 @@
 </template>
 
 <script>
-import 'assets/push-notifications.js'
 export default {
-  data () {
-    return {
-      phoneverified: this.$q.localStorage.getItem('JOURNEY_VerifiedPhone'),
-      grace: false,
-      persistent: false,
-      socmodal: false,
-      societies: [],
-      filteredSocieties: [],
-      denomination: 1,
-      mySociety: null,
-      church: 'mcsa'
-    }
-  },
-  computed: {
-    society () {
-      if (this.$q.localStorage.getItem('JOURNEY_Society')) {
-        return true
-      } else {
-        return false
-      }
-    },
-    permission () {
-      if (!this.$q.platform.is.ios) {
-        return Notification.permission
-      } else {
-        return 'unavailable'
-      }
-    }
-  },
-  async mounted () {
-    if (!this.$store.state.individual || !this.$store.state.individual.id) {
-      if (this.$q.localStorage.getItem('JOURNEY_Individual')) {
-        this.$store.commit('setIndividual', this.$q.localStorage.getItem('JOURNEY_Individual'))
-      } else {
-        this.getSocieties()
-      }
-    }
-  },
-  methods: {
-    getSocieties () {
-      this.$axios.get(process.env.API + '/denominations/' + this.denomination + '/societies')
-        .then(response => {
-          for (var soc in response.data) {
-            this.societies.push({
-              label: response.data[soc].society + ' (' + response.data[soc].circuit + ', ' + response.data[soc].district + ')',
-              value: response.data[soc].society_id,
-              society: response.data[soc].society,
-              circuit: response.data[soc].circuit,
-              circuit_id: response.data[soc].circuit_id,
-              district: response.data[soc].district_id
-            })
-          }
-        })
-        .catch(error => {
-          if (error.code === 'ECONNABORTED') {
-            this.$q.notify('Server connection timed out - are you offline?')
-          } else {
-            console.log(error)
-          }
-        })
-    },
-    setSociety () {
-      this.socmodal = false
-      this.$q.localStorage.set('JOURNEY_Societyname', this.mySociety.society)
-      this.$q.localStorage.set('JOURNEY_Circuitname', this.mySociety.circuit)
-      this.$q.localStorage.set('JOURNEY_Society', this.mySociety.value)
-      this.$q.localStorage.set('JOURNEY_Circuit', this.mySociety.circuit_id)
-      this.$q.localStorage.set('JOURNEY_District', this.mySociety.district)
-      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
-      this.$axios.post(process.env.API + '/phone',
-        {
-          phone: this.$q.localStorage.getItem('JOURNEY_VerifiedPhone'),
-          society_id: this.$q.localStorage.getItem('JOURNEY_Society')
-        })
-        .then(response => {
-          if (response.data.individual) {
-            if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
-              this.$q.localStorage.set('JOURNEY_Individual', JSON.stringify(response.data.individual))
-            }
-          }
-          window.location.reload()
-        })
-        .catch(function (error) {
-          if (error.response.status === 401) {
-            localStorage.removeItem('JOURNEY_Token')
-            window.location.reload()
-          } else {
-            console.log(error)
-          }
-        })
-    },
-    filterFn (val, update, abort) {
-      if (val.length < 2) {
-        abort()
-        return
-      }
-      update(() => {
-        const needle = val.toLowerCase()
-        this.filteredSocieties = this.societies.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
-      })
-    }
-  }
 }
 </script>
 
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -165,11 +165,69 @@
       </q-list>
     </q-drawer>
     <q-page-container>
-      <div v-if="loading" class="text-center bg-grey-4 text-white">
-        <div v-if="!offerrefresh" class="q-pa-md" transition-hide="fade">
-          Checking for new content <q-spinner-bars class="q-ml-sm" size="20px" color="white" />
+      <div v-if="!phoneverified" class="text-center q-ma-md">
+        <q-btn color="secondary" to="phoneverification">Please verify your phone number</q-btn>
+        <q-btn class="q-ml-sm" label="Why?" color="primary" @click="persistent = true" />
+        <q-dialog v-model="persistent" persistent transition-show="scale" transition-hide="scale">
+          <q-card class="bg-primary text-white">
+            <q-card-section>
+              You don't have to verify your number, but if you do, you'll be able to:<br><br>
+              <li>update your contact details,</li>
+              <li>sign up for events*,</li>
+              <li>view profile pictures of members of your congregation*</li>
+              <li>receive reminders when you are on duty or preaching on a Sunday*, and</li>
+              <li>view your personal giving records.*</li>
+              <li>In addition, you can subscribe to additional blogs / sermon podcasts</li><br>
+              <small>*these features assume that someone in your church is set up as an administrator using the ChurchNet system.</small>
+            </q-card-section>
+            <q-card-actions align="center" class="bg-white text-teal">
+              <q-btn flat label="OK" v-close-popup />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
+      </div>
+      <div v-else>
+        <div v-if="!society" class="text-center q-ma-md">
+          <q-btn @click="socmodal = true" color="secondary">Which church do you belong to?</q-btn>
         </div>
-        <div v-else class="q-py-md text-black">Switching to latest content in {{countdown}}s</div>
+        <div v-else-if="loading" class="text-center bg-grey-4 text-white">
+          <div v-if="!offerrefresh" class="q-pa-md" transition-hide="fade">
+            Checking for new content <q-spinner-bars class="q-ml-sm" size="20px" color="white" />
+          </div>
+          <div v-else class="q-py-md text-black">Switching to latest content in {{countdown}}s</div>
+        </div>
+        <div v-if="noindiv && society && !$q.localStorage.getItem('JOURNEY_Individual')">
+          <div class="text-center q-ma-md">
+            Welcome, {{phoneverified}}<br>
+            <q-btn @click="noindiv = false" to="adduser" color="secondary">Update my details</q-btn>
+          </div>
+        </div>
+        <q-dialog v-model="socmodal" persistent transition-show="scale" transition-hide="scale">
+          <q-card class="bg-primary text-white" style="width: 400px">
+            <q-card-section>
+              <div class="text-h6">Find your church</div>
+            </q-card-section>
+            <q-card-section class="bg-white">
+              <div class="q-gutter-md row">
+                <q-select outlined v-model="church" label="Church" :options="[{label: 'Methodist Church of Southern Africa', value: 'mcsa'}]" style="width:100%;" emit-value map-options/>
+                <q-select autofocus placeholder="Type here to find or change your church" outlined v-model="mySociety" use-input input-debounce="0" :options="filteredSocieties" @filter="filterFn" clearable style="width:100%;">
+                  <template v-slot:no-option>
+                    <q-item>
+                      <q-item-section class="text-grey">
+                        No results
+                      </q-item-section>
+                    </q-item>
+                  </template>
+                </q-select>
+              </div>
+            </q-card-section>
+            <q-card-actions align="right" class="bg-white text-black">
+              <q-btn class="bg-white text-primary" to="addchurch" flat label="I can't find my church" v-close-popup/>
+              <q-btn class="bg-grey text-white" flat label="Cancel" v-close-popup/>
+              <q-btn class="bg-primary text-white" flat label="OK" @click="setSociety"/>
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
       </div>
       <router-view />
     </q-page-container>
@@ -213,32 +271,86 @@ export default {
       expanded: true,
       countdown: 3,
       newdata: [],
-      menus: ['blog', 'devotional', 'diary', 'events', 'groups', 'media', 'practice', 'reminders', 'sermon']
+      menus: ['blog', 'devotional', 'diary', 'events', 'groups', 'media', 'practice', 'reminders', 'sermon'],
+      persistent: false,
+      socmodal: false,
+      societies: [],
+      filteredSocieties: [],
+      denomination: 1,
+      mySociety: null,
+      noindiv: false,
+      church: 'mcsa'
+    }
+  },
+  computed: {
+    society () {
+      if (this.$q.localStorage.getItem('JOURNEY_Society')) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  mounted () {
+    // First, deal with possible upgrades
+    if (this.$q.localStorage.getItem('JOURNEY_Version')) {
+      if (this.$q.localStorage.getItem('JOURNEY_Version') !== process.env.VERSION) {
+        this.$q.dialog({
+          title: 'New version available',
+          message: 'Click OK to restart the app and upgrade to version ' + process.env.VERSION + '. This new version includes: ' + process.env.VNOTES,
+          ok: 'OK',
+          cancel: 'LATER'
+        }).onOk(() => {
+          this.$q.localStorage.set('JOURNEY_Version', process.env.VERSION)
+          window.location.reload()
+        }).catch(() => {
+          console.log('Delaying upgrade')
+        })
+      }
+    } else {
+      this.$q.localStorage.set('JOURNEY_Version', process.env.VERSION)
+    }
+
+    // Settings housekeeping - populating store
+    if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
+      this.noindiv = true
+    }
+    if (this.$q.localStorage.getItem('JOURNEY_Circuit')) {
+      this.$store.commit('setSocietyName', this.$q.localStorage.getItem('JOURNEY_Societyname'))
+      this.$store.commit('setSocietyId', this.$q.localStorage.getItem('JOURNEY_Society'))
+      this.$store.commit('setCircuitId', this.$q.localStorage.getItem('JOURNEY_Circuit'))
+      this.$store.commit('setCircuitName', this.$q.localStorage.getItem('JOURNEY_Circuitname'))
+    }
+    if (this.$q.localStorage.getItem('JOURNEY_VerifiedPhone')) {
+      if (!this.$q.localStorage.getItem('JOURNEY_Token')) {
+        this.get_token()
+      } else {
+        this.$store.commit('setToken', this.$q.localStorage.getItem('JOURNEY_Token'))
+        this.getindiv()
+      }
     }
   },
   methods: {
-    async get_token () {
-      if (this.$q.localStorage.getItem('JOURNEY_VerifiedPhone')) {
-        this.$axios.post(process.env.API + '/login',
-          {
-            phone: this.$q.localStorage.getItem('JOURNEY_VerifiedPhone'),
-            phonetoken: this.$q.localStorage.getItem('JOURNEY_Phonetoken')
-          })
-          .then(response => {
-            this.$q.localStorage.set('JOURNEY_Token', response.data.token)
-            this.$store.commit('setToken', response.data.token)
-            if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
-              this.getindiv()
-            }
-          })
-          .catch(error => {
-            if (error.code === 'ECONNABORTED') {
-              this.$q.notify('Server connection timed out - are you offline?')
-            } else {
-              console.log(error)
-            }
-          })
-      }
+    get_token () {
+      this.$axios.post(process.env.API + '/login',
+        {
+          phone: this.$q.localStorage.getItem('JOURNEY_VerifiedPhone'),
+          phonetoken: this.$q.localStorage.getItem('JOURNEY_Phonetoken')
+        })
+        .then(response => {
+          this.$q.localStorage.set('JOURNEY_Token', response.data.token)
+          this.$store.commit('setToken', response.data.token)
+          if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
+            this.getindiv()
+          }
+        })
+        .catch(error => {
+          if (error.code === 'ECONNABORTED') {
+            this.$q.notify('Server connection timed out - are you offline?')
+          } else {
+            console.log(error)
+          }
+        })
     },
     updateFeed () {
       this.$store.commit('setFeeditems', this.newdata)
@@ -299,6 +411,68 @@ export default {
           }
         })
     },
+    getSocieties () {
+      this.$axios.get(process.env.API + '/denominations/' + this.denomination + '/societies')
+        .then(response => {
+          for (var soc in response.data) {
+            this.societies.push({
+              label: response.data[soc].society + ' (' + response.data[soc].circuit + ', ' + response.data[soc].district + ')',
+              value: response.data[soc].society_id,
+              society: response.data[soc].society,
+              circuit: response.data[soc].circuit,
+              circuit_id: response.data[soc].circuit_id,
+              district: response.data[soc].district_id
+            })
+          }
+        })
+        .catch(error => {
+          if (error.code === 'ECONNABORTED') {
+            this.$q.notify('Server connection timed out - are you offline?')
+          } else {
+            console.log(error)
+          }
+        })
+    },
+    setSociety () {
+      this.socmodal = false
+      this.$q.localStorage.set('JOURNEY_Societyname', this.mySociety.society)
+      this.$q.localStorage.set('JOURNEY_Circuitname', this.mySociety.circuit)
+      this.$q.localStorage.set('JOURNEY_Society', this.mySociety.value)
+      this.$q.localStorage.set('JOURNEY_Circuit', this.mySociety.circuit_id)
+      this.$q.localStorage.set('JOURNEY_District', this.mySociety.district)
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
+      this.$axios.post(process.env.API + '/phone',
+        {
+          phone: this.$q.localStorage.getItem('JOURNEY_VerifiedPhone'),
+          society_id: this.$q.localStorage.getItem('JOURNEY_Society')
+        })
+        .then(response => {
+          if (response.data.individual) {
+            if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
+              this.$q.localStorage.set('JOURNEY_Individual', JSON.stringify(response.data.individual))
+            }
+          }
+          window.location.reload()
+        })
+        .catch(function (error) {
+          if (error.response.status === 401) {
+            localStorage.removeItem('JOURNEY_Token')
+            window.location.reload()
+          } else {
+            console.log(error)
+          }
+        })
+    },
+    filterFn (val, update, abort) {
+      if (val.length < 2) {
+        abort()
+        return
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        this.filteredSocieties = this.societies.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    },
     getindiv () {
       this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
       this.$axios.post(process.env.API + '/phone',
@@ -309,12 +483,13 @@ export default {
         .then(response => {
           if (response.data.individual) {
             this.$store.commit('setIndividual', response.data.individual)
-            if (!this.$q.localStorage.getItem('JOURNEY_Individual')) {
-              this.$q.localStorage.set('JOURNEY_Individual', JSON.stringify(response.data.individual))
-            }
+            this.$q.localStorage.set('JOURNEY_Individual', JSON.stringify(response.data.individual))
+            this.noindiv = false
           }
           if (this.$store.state.societyid) {
             this.getfeedcontent()
+          } else {
+            this.getSocieties()
           }
         })
         .catch(function (error) {
@@ -325,40 +500,6 @@ export default {
             console.log(error)
           }
         })
-    }
-  },
-  async mounted () {
-    // Deal with upgrades
-    this.timeout = false
-    if (this.$q.localStorage.getItem('JOURNEY_Version')) {
-      if (this.$q.localStorage.getItem('JOURNEY_Version') !== process.env.VERSION) {
-        this.$q.dialog({
-          title: 'New version available',
-          message: 'Click OK to restart the app and upgrade to version ' + process.env.VERSION + '. This new version includes: ' + process.env.VNOTES,
-          ok: 'OK',
-          cancel: 'LATER'
-        }).onOk(() => {
-          this.$q.localStorage.set('JOURNEY_Version', process.env.VERSION)
-          window.location.reload()
-        }).catch(() => {
-          console.log('Delaying upgrade')
-        })
-      }
-    } else {
-      this.$q.localStorage.set('JOURNEY_Version', process.env.VERSION)
-    }
-    // Check that settings are intact
-    if (this.$q.localStorage.getItem('JOURNEY_Circuit')) {
-      this.$store.commit('setSocietyName', this.$q.localStorage.getItem('JOURNEY_Societyname'))
-      this.$store.commit('setSocietyId', this.$q.localStorage.getItem('JOURNEY_Society'))
-      this.$store.commit('setCircuitId', this.$q.localStorage.getItem('JOURNEY_Circuit'))
-      this.$store.commit('setCircuitName', this.$q.localStorage.getItem('JOURNEY_Circuitname'))
-    }
-    if (!this.$q.localStorage.getItem('JOURNEY_Token')) {
-      await this.get_token()
-    } else {
-      this.$store.commit('setToken', this.$q.localStorage.getItem('JOURNEY_Token'))
-      this.getindiv()
     }
   }
 }
